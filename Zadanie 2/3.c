003c40ecce7b8a47b41074356b38bb449773d376
Kacper Wójcik <147393505+bigdumdum1@users.noreply.github.com>
2024-03-05 12:14:14 +0100

    Update 3.c

--- Zadanie 2/3.c
+++ Zadanie 2/3.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <math.h>
 
+struct Point {
+  float x;
+  float y;
+}
+
+float Func(float x, float y){
+    float* pnt = &x;
+    
+    *pnt = *pnt * 2;
+    
+    float* pnt1 = &y;
+    
+    *pnt1 = *pnt1 * 2;
+    
+    return *pnt, *pnt1;
+}
+struct Rectangle {
+
+}
+
+int main()
+{
+
+}
