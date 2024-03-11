49938e62ed76e37a5a45414004bf9cc1af6c2f95
Kacper Wójcik <147393505+bigdumdum1@users.noreply.github.com>
2024-03-11 20:44:50 +0100

    Update 1.c

--- Zadanie3/1.c
+++ Zadanie3/1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int pascal(int row, int col) {
+    if (col == 0 || col == row)
+        return 1;
+    else
+        return pascal(row - 1, col - 1) + pascal(row - 1, col);
+};
+
+void pascaltriangle(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", pascal(i, j));
+        };
+        printf("\n");
+    };
+};
+
+int main() {
+    int height = 9;
+
+    pascaltriangle(height);
+
+    return 0;
+};
