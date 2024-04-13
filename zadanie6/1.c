ee021bd102fa83458c4d3ee87f345c212aed2a45
Kacper Wójcik <147393505+bigdumdum1@users.noreply.github.com>
2024-04-13 23:18:46 +0200

    Update 1.c

--- zadanie6/1.c
+++ zadanie6/1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Definicja unii
 union Dane {
     int calkowita;
     float zmiennoprzecinkowa;
@@ -8,18 +7,14 @@ union Dane {
 };
 
 int main() {
-    // Inicjalizacja unii
     union Dane dane;
-
-    // Przechowywanie i wyświetlanie wartości typu int
+    
     dane.calkowita = 10;
     printf("Wartość typu int: %d\n", dane.calkowita);
 
-    // Przechowywanie i wyświetlanie wartości typu float
     dane.zmiennoprzecinkowa = 3.14;
     printf("Wartość typu float: %f\n", dane.zmiennoprzecinkowa);
 
-    // Przechowywanie i wyświetlanie wartości typu char
     dane.znak = 'A';
     printf("Wartość typu char: %c\n", dane.znak);
 
