d64a89d78d2a348e5633da08d851463333e0e9f1
Kacper Wójcik <147393505+bigdumdum1@users.noreply.github.com>
2024-04-13 23:18:32 +0200

    Create 2.c

---
+++ zadanie6/2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int tablica[10];
+
+    for (int i = 0; i < 10; i++) {
+        tablica[i] = i;
+    }
+
+    int *wskaznik = tablica;
+
+    printf("Zawartość tablicy:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", *wskaznik);
+        wskaznik++;
+    }
+    printf("\n");
+
+    return 0;
+}
